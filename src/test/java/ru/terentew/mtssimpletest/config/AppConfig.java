39ef5dca8c5dfe93dc9d6d6aaba46e45d9737c93
Alexander Terentev <33878295+winnerrr@users.noreply.github.com>
2022-02-23 13:02:20 +0300

    no demo2

--- src/test/java/ru/terentew/mtssimpletest/config/AppConfig.java
+++ src/test/java/ru/terentew/mtssimpletest/config/AppConfig.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:config/demowebshop/app.properties"
+        "classpath:config/app.properties"
 })
 public interface AppConfig extends Config {
 
