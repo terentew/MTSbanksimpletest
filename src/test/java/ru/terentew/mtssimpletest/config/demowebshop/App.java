3a5b45c4919f34e1b9453606a9225ed767d57f79
Alexander Terentev <33878295+winnerrr@users.noreply.github.com>
2022-02-23 11:56:12 +0300

    no demo

--- src/test/java/ru/terentew/mtssimpletest/config/demowebshop/App.java
+++
@@ -1,7 +0,0 @@
-package ru.terentew.mtssimpletest.config.demowebshop;
-
-import org.aeonbits.owner.ConfigFactory;
-
-public class App {
-    public static AppConfig config = ConfigFactory.create(AppConfig.class, System.getProperties());
-}
